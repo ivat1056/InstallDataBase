064cca5889deebfbc5fc98cfce17e2652383ffa0
ivan matveev <you@gmail.com>
2023-03-02 23:05:52 +0300

    Добавьте файлы проекта.

---
+++ proba/Class/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proba.Class
+{
+    internal class Base
+    {
+        public static EP ep;
+    }
+}
